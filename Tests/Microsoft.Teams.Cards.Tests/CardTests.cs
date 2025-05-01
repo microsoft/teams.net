22378569deeb3cbdb645d31cdd204a42860c7b96
Falguni Thakor <fthakor@microsoft.com>
2025-05-01 10:18:32 -0700

    commiting current state of card test

---
+++ Tests/Microsoft.Teams.Cards.Tests/CardTests.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+
+namespace Microsoft.Teams.Cards.Tests;
+public class CardTests
+{
+    [Fact]
+    public void validateCardDefault()
+    {
+        
+        var card = new Card();
+
+        var json = JsonSerializer.Serialize(card, new JsonSerializerOptions()
+        {
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+        var exectedObject = new
+        {
+            type = "AdaptiveCard",
+            version="1.6"
+        };
+        var exectedObjectJson = JsonSerializer.Serialize(exectedObject, new JsonSerializerOptions()
+        {
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        Assert.Equal(exectedObjectJson, json);
+    }
+}
