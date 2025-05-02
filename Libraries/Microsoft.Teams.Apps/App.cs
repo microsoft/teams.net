ad4961708192f139e6d6b7fb394b00daf385f4f1
Alex Acebo <aacebowork@gmail.com>
2025-05-02 10:53:52 -0700

    add public method to manually process activity

--- Libraries/Microsoft.Teams.Apps/App.cs
+++ Libraries/Microsoft.Teams.Apps/App.cs
@@ -209,4 +209,131 @@ public partial class App : IApp
     {
         return await Send(conversationId, new MessageActivity().AddAttachment(card), serviceUrl, cancellationToken);
     }
+
+    /// <summary>
+    /// process an activity
+    /// </summary>
+    /// <param name="sender">the plugin to use</param>
+    /// <param name="token">the request token</param>
+    /// <param name="activity">the inbound activity</param>
+    /// <param name="cancellationToken">the cancellation token</param>
+    public async Task<Response> Process(ISenderPlugin sender, IToken token, IActivity activity, CancellationToken cancellationToken = default)
+    {
+        var routes = Router.Select(activity);
+        JsonWebToken? userToken = null;
+
+        var api = new ApiClient(Api);
+
+        try
+        {
+            var tokenResponse = await api.Users.Token.GetAsync(new()
+            {
+                UserId = activity.From.Id,
+                ChannelId = activity.ChannelId,
+                ConnectionName = "graph"
+            });
+
+            userToken = new JsonWebToken(tokenResponse);
+        }
+        catch { }
+
+        var path = activity.GetPath();
+        Logger.Debug(path);
+
+        var reference = new ConversationReference()
+        {
+            ServiceUrl = activity.ServiceUrl ?? token.ServiceUrl,
+            ChannelId = activity.ChannelId,
+            Bot = activity.Recipient,
+            User = activity.From,
+            Locale = activity.Locale,
+            Conversation = activity.Conversation,
+        };
+
+        var userGraphTokenProvider = Azure.Core.DelegatedTokenCredential.Create((context, _) =>
+        {
+            return userToken == null ? default : new Azure.Core.AccessToken(userToken.ToString(), userToken.Token.ValidTo);
+        });
+
+        object? data = null;
+        var i = -1;
+        async Task<object?> Next(IContext<IActivity> context)
+        {
+            i++;
+            if (i == routes.Count) return data;
+            var res = await routes[i].Invoke(context);
+
+            if (res != null)
+                data = res;
+
+            return res;
+        }
+
+        var stream = sender.CreateStream(reference, cancellationToken);
+        var context = new Context<IActivity>(sender, stream)
+        {
+            AppId = token.AppId ?? Id ?? string.Empty,
+            Log = Logger.Child(path),
+            Storage = Storage,
+            Api = api,
+            Activity = activity,
+            Ref = reference,
+            IsSignedIn = userToken != null,
+            OnNext = Next,
+            UserGraph = new Graph.GraphServiceClient(userGraphTokenProvider),
+            CancellationToken = cancellationToken,
+            OnActivitySent = (activity, context) => ActivitySentEvent(this, activity, context)
+        };
+
+        stream.OnChunk += activity => ActivitySentEvent(this, activity, context);
+
+        try
+        {
+            await ActivityEvent(this, context);
+
+            foreach (var plugin in Plugins)
+            {
+                await plugin.OnActivity(this, context);
+            }
+
+            var res = await Next(context);
+            await stream.Close();
+
+            var response = res is Response value ? value : new Response(System.Net.HttpStatusCode.OK, res);
+            await ActivityResponseEvent(this, response, context);
+            return response;
+        }
+        catch (Exception err)
+        {
+            await ErrorEvent(this, sender, err, context);
+            return new Response(System.Net.HttpStatusCode.InternalServerError);
+        }
+    }
+
+    /// <summary>
+    /// process an activity
+    /// </summary>
+    /// <param name="sender">the plugin to use</param>
+    /// <param name="token">the request token</param>
+    /// <param name="activity">the inbound activity</param>
+    /// <param name="cancellationToken">the cancellation token</param>
+    /// <exception cref="Exception"></exception>
+    public Task<Response> Process(string sender, IToken token, IActivity activity, CancellationToken cancellationToken = default)
+    {
+        var plugin = ((ISenderPlugin?)GetPlugin(sender)) ?? throw new Exception($"sender plugin '{sender}' not found");
+        return Process(plugin, token, activity, cancellationToken);
+    }
+
+    /// <summary>
+    /// process an activity
+    /// </summary>
+    /// <param name="token">the request token</param>
+    /// <param name="activity">the inbound activity</param>
+    /// <param name="cancellationToken">the cancellation token</param>
+    /// <exception cref="Exception"></exception>
+    public Task<Response> Process<TPlugin>(IToken token, IActivity activity, CancellationToken cancellationToken = default) where TPlugin : ISenderPlugin
+    {
+        var plugin = GetPlugin<TPlugin>() ?? throw new Exception($"sender plugin '{typeof(TPlugin).Name}' not found");
+        return Process(plugin, token, activity, cancellationToken);
+    }
 }
--- Libraries/Microsoft.Teams.Apps/AppEvents.cs
+++ Libraries/Microsoft.Teams.Apps/AppEvents.cs
@@ -1,7 +1,6 @@
 using Microsoft.Teams.Api;
 using Microsoft.Teams.Api.Activities;
 using Microsoft.Teams.Api.Auth;
-using Microsoft.Teams.Api.Clients;
 using Microsoft.Teams.Apps.Plugins;
 using Microsoft.Teams.Common.Http;
 using Microsoft.Teams.Common.Logging;
@@ -119,96 +118,8 @@ public partial class App
         }
     }
 
-    protected async Task<Response> OnActivityEvent(ISenderPlugin sender, IToken token, IActivity activity, CancellationToken cancellationToken = default)
+    protected Task<Response> OnActivityEvent(ISenderPlugin sender, IToken token, IActivity activity, CancellationToken cancellationToken = default)
     {
-        var routes = Router.Select(activity);
-        JsonWebToken? userToken = null;
-
-        var api = new ApiClient(Api);
-
-        try
-        {
-            var tokenResponse = await api.Users.Token.GetAsync(new()
-            {
-                UserId = activity.From.Id,
-                ChannelId = activity.ChannelId,
-                ConnectionName = "graph"
-            });
-
-            userToken = new JsonWebToken(tokenResponse);
-        }
-        catch { }
-
-        var path = activity.GetPath();
-        Logger.Debug(path);
-
-        var reference = new ConversationReference()
-        {
-            ServiceUrl = activity.ServiceUrl ?? token.ServiceUrl,
-            ChannelId = activity.ChannelId,
-            Bot = activity.Recipient,
-            User = activity.From,
-            Locale = activity.Locale,
-            Conversation = activity.Conversation,
-        };
-
-        var userGraphTokenProvider = Azure.Core.DelegatedTokenCredential.Create((context, _) =>
-        {
-            return userToken == null ? default : new Azure.Core.AccessToken(userToken.ToString(), userToken.Token.ValidTo);
-        });
-
-        object? data = null;
-        var i = -1;
-        async Task<object?> Next(IContext<IActivity> context)
-        {
-            i++;
-            if (i == routes.Count) return data;
-            var res = await routes[i].Invoke(context);
-
-            if (res != null)
-                data = res;
-
-            return res;
-        }
-
-        var stream = sender.CreateStream(reference, cancellationToken);
-        var context = new Context<IActivity>(sender, stream)
-        {
-            AppId = token.AppId ?? Id ?? string.Empty,
-            Log = Logger.Child(path),
-            Storage = Storage,
-            Api = api,
-            Activity = activity,
-            Ref = reference,
-            IsSignedIn = userToken != null,
-            OnNext = Next,
-            UserGraph = new Graph.GraphServiceClient(userGraphTokenProvider),
-            CancellationToken = cancellationToken,
-            OnActivitySent = (activity, context) => ActivitySentEvent(this, activity, context)
-        };
-
-        stream.OnChunk += activity => ActivitySentEvent(this, activity, context);
-
-        try
-        {
-            await ActivityEvent(this, context);
-
-            foreach (var plugin in Plugins)
-            {
-                await plugin.OnActivity(this, context);
-            }
-
-            var res = await Next(context);
-            await stream.Close();
-
-            var response = res is Response value ? value : new Response(System.Net.HttpStatusCode.OK, res);
-            await ActivityResponseEvent(this, response, context);
-            return response;
-        }
-        catch (Exception err)
-        {
-            await ErrorEvent(this, sender, err, context);
-            return new Response(System.Net.HttpStatusCode.InternalServerError);
-        }
+        return Process(sender, token, activity, cancellationToken); 
     }
 }
