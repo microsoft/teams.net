b76a8a139148481ff9369a97d990470528bac36a
Falguni Thakor <fthakor@microsoft.com>
2025-05-08 09:16:12 -0700

    fixed Handoff tests - missing serialize/deserialize fun in InvokeActivity.JsonConverter

--- Tests/Microsoft.Teams.Api.Tests/Activities/Invokes/HandoffInvokeActivityTests.cs
+++ Tests/Microsoft.Teams.Api.Tests/Activities/Invokes/HandoffInvokeActivityTests.cs
@@ -32,68 +32,93 @@ public class HandoffInvokeActivityTests
         ), json);
     }
 
-    //[Fact]
-    //public void HandoffInvokeActivity_JsonSerialize_Derived()
-    //{
-    //    InvokeActivity activity = new HandoffActivity()
-    //    {
-    //        Value = new HandoffActivityValue() { Continuation = " valid continuation" }
-    //    };
+    [Fact]
+    public void HandoffInvokeActivity_JsonSerialize_Derived()
+    {
+        InvokeActivity activity = new HandoffActivity()
+        {
+            Id = "handoffId",
+            ChannelId = new ChannelId("channelId"),
+            Value = new HandoffActivityValue() { Continuation = " valid continuation" }
+        };
 
-    //    var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
-    //    {
-    //        WriteIndented = true,
-    //        IndentSize = 2,
-    //        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-    //    });
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
 
-    //    Assert.Equal(File.ReadAllText(
-    //        @"../../../Json/Activity/Invokes/HandoffActivity.json"
-    //    ), json);
-    //}
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/HandoffActivity.json"
+        ), json);
+    }
 
-    //[Fact]
-    //public void HandoffInvokeActivity_JsonSerialize_Interface_Derived()
-    //{
-    //    Activity activity = new HandoffActivity()
-    //    {
-    //        Value = new HandoffActivityValue() { Continuation = " valid continuation" }
-    //    };
+    [Fact]
+    public void HandoffInvokeActivity_JsonSerialize_Interface_Derived()
+    {
+        Activity activity = new HandoffActivity()
+        {
+            Id = "handoffId",
+            ChannelId = new ChannelId("channelId"),
+            Value = new HandoffActivityValue() { Continuation = " valid continuation" }
+        };
 
-    //    var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
-    //    {
-    //        WriteIndented = true,
-    //        IndentSize = 2,
-    //        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-    //    });
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
 
-    //    Assert.Equal(File.ReadAllText(
-    //        @"../../../Json/Activity/Invokes/HandoffActivity.json"
-    //    ), json);
-    //}
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/HandoffActivity.json"
+        ), json);
+    }
 
 
-    //[Fact]
-    //public void HandoffInvokeActivity_JsonDeserialize()
-    //{
-    //   var json = File.ReadAllText(@"../../../Json/Activity/Invokes/HandoffActivity.json");
-    //   var activity = JsonSerializer.Deserialize<HandoffActivity>(json);
-    //   var expected = new InvokeActivity(Name.Handoff);
-    //   Assert.Equal(expected.ToString(), activity.ToString());
-    //}
+    [Fact]
+    public void HandoffInvokeActivity_JsonDeserialize()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/HandoffActivity.json");
+        var activity = JsonSerializer.Deserialize<HandoffActivity>(json);
+        var expected = new HandoffActivity()
+        {
+            Id = "handoffId",
+            ChannelId = new ChannelId("channelId"),
+            Value = new HandoffActivityValue() { Continuation = " valid continuation" }
+        };
+        Assert.Equal(expected.ToString(), activity.ToString());
+    }
 
-    //[Fact]
-    //public void HandoffInvokeActivity_JsonDeserialize_Derived()
-    //{
-    //   var json = File.ReadAllText(@"../../../Json/Activity/Invokes/HandoffActivity.json");
-    //   var activity = JsonSerializer.Deserialize<InvokeActivity>(json);
-    //   var expected = new HandoffActivity()
-    //   {
-    //       Value = new HandoffActivityValue() { Continuation = "hand off continuation" }
-    //   };
+    [Fact]
+    public void HandoffInvokeActivity_JsonDeserialize_Derived()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/HandoffActivity.json");
+        var activity = JsonSerializer.Deserialize<InvokeActivity>(json);
+        var expected = new HandoffActivity()
+        {
+            Id = "handoffId",
+            ChannelId = new ChannelId("channelId"),
+            Value = new HandoffActivityValue() { Continuation = " valid continuation" }
+        };
 
-    //   Assert.Equal(expected.ToString(), activity.ToString());
-    //}
+      Assert.Equal(expected.ToString(), activity.ToString());
+    }
 
+    [Fact]
+    public void HandoffInvokeActivity_JsonDeserialize_Interface_Derived()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/HandoffActivity.json");
+        var activity = JsonSerializer.Deserialize<Activity>(json);
+        var expected = new HandoffActivity()
+        {
+            Id = "handoffId",
+            ChannelId = new ChannelId("channelId"),
+            Value = new HandoffActivityValue() { Continuation = " valid continuation" }
+        };
+
+        Assert.Equal(expected.ToString(), activity.ToString());
+    }
 
 }
