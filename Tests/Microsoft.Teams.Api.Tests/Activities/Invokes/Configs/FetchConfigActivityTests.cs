19d11d1e08ffbc29c576c03dcdd448ad5624ff1d
Falguni Thakor <fthakor@microsoft.com>
2025-05-09 15:00:35 -0700

    added Configs Invoke activity test coverage

---
+++ Tests/Microsoft.Teams.Api.Tests/Activities/Invokes/Configs/FetchConfigActivityTests.cs
@@ -0,0 +1,137 @@
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+using Microsoft.Teams.Api.Activities;
+using Microsoft.Teams.Api.Activities.Invokes;
+
+using static Microsoft.Teams.Api.Activities.Invokes.Configs;
+
+namespace Microsoft.Teams.Api.Tests.Activities.Invokes.Configs;
+
+public class ConfigFetchActivityTests
+{
+    private FetchActivity setupConfigFetchActivity()
+    {
+        var value = new Cards.HeroCard()
+        {
+            Title = "test card",
+            SubTitle = "test fetch config activity"
+        };
+        return new FetchActivity(value);
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonSerialize()
+    {
+        var activity = setupConfigFetchActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        string expectedPath = "Activity.Invoke.Config/fetch";
+        Assert.Equal(expectedPath, activity.GetPath());
+
+        Assert.NotNull(activity.ToFetch());
+        var expectedSubmitException = "Unable to cast object of type 'FetchActivity' to type 'SubmitActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToSubmit());
+        Assert.Equal(expectedSubmitException, ex.Message);
+
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigFetchActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonSerialize_Derived()
+    {
+        ConfigActivity activity = setupConfigFetchActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        string expectedPath = "Activity.Invoke.Config/fetch";
+        Assert.Equal(expectedPath, activity.GetPath());
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigFetchActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonSerialize_Derived_Interface()
+    {
+        InvokeActivity activity = setupConfigFetchActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+        
+        string expectedPath = "Activity.Invoke.Config/fetch";
+        Assert.Equal(expectedPath, activity.GetPath());
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigFetchActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonDeserialize()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigFetchActivity.json");
+        var activity = JsonSerializer.Deserialize<SubmitActivity>(json);
+        var expected = setupConfigFetchActivity();
+
+        Assert.Equal(expected.ToString(), activity.ToString());
+        Assert.NotNull(activity.ToConfig());
+
+        var expectedSubmitException = "Unable to cast object of type 'SubmitActivity' to type 'Microsoft.Teams.Api.Activities.Invokes.TaskActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToTask());
+        Assert.Equal(expectedSubmitException, ex.Message);
+    }
+
+    [Fact]
+    public void ConfigFetchActivityJsonDeserialize_Derived()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigFetchActivity.json");
+        var activity = JsonSerializer.Deserialize<ConfigActivity>(json);
+        var expected = setupConfigFetchActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+        Assert.NotNull(activity.ToFetch());
+        var expectedSubmitException = "Unable to cast object of type 'FetchActivity' to type 'SubmitActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToSubmit());
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonDeserialize_Derived_Interface()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigFetchActivity.json");
+        var activity = JsonSerializer.Deserialize<InvokeActivity>(json);
+        var expected = setupConfigFetchActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+
+    }
+
+    [Fact]
+    public void ConfigFetchActivity_JsonDeserialize_Derived_Activity_Interface()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigFetchActivity.json");
+        var activity = JsonSerializer.Deserialize<Activity>(json);
+        var expected = setupConfigFetchActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+    }
+}
---
+++ Tests/Microsoft.Teams.Api.Tests/Activities/Invokes/Configs/SubmitConfigActivityTests.cs
@@ -0,0 +1,129 @@
+using System.Text.Json;
+
+using Microsoft.Teams.Api.Activities;
+using Microsoft.Teams.Api.Activities.Invokes;
+
+using static Microsoft.Teams.Api.Activities.Invokes.Configs;
+
+namespace Microsoft.Teams.Api.Tests.Activities.Invokes.Configs;
+
+public class ConfigSubmitActivityTests
+{
+    private SubmitActivity setupConfigSubmitActivity()
+    {
+        var value = "You have chosen to submit config for bot";
+        return new SubmitActivity(value);
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonSerialize()
+    {
+        var activity = setupConfigSubmitActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        string expectedPath = "Activity.Invoke.Config/submit";
+        Assert.Equal(expectedPath, activity.GetPath());
+
+        Assert.NotNull(activity.ToSubmit());
+        var expectedSubmitException = "Unable to cast object of type 'SubmitActivity' to type 'FetchActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToFetch());
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigSubmitActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonSerialize_Derived()
+    {
+        ConfigActivity activity = setupConfigSubmitActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        string expectedPath = "Activity.Invoke.Config/submit";
+        Assert.Equal(expectedPath, activity.GetPath());
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigSubmitActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonSerialize_Derived_Interface()
+    {
+        InvokeActivity activity = setupConfigSubmitActivity();
+
+        var json = JsonSerializer.Serialize(activity, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            IndentSize = 2,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        });
+
+        string expectedPath = "Activity.Invoke.Config/submit";
+        Assert.Equal(expectedPath, activity.GetPath());
+        Assert.Equal(File.ReadAllText(
+            @"../../../Json/Activity/Invokes/ConfigSubmitActivity.json"
+        ), json);
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonDeserialize()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigSubmitActivity.json");
+        var activity = JsonSerializer.Deserialize<SubmitActivity>(json);
+        var expected = setupConfigSubmitActivity();
+
+        Assert.Equal(expected.ToString(), activity.ToString());
+        Assert.NotNull(activity.ToConfig());
+
+        var expectedSubmitException = "Unable to cast object of type 'SubmitActivity' to type 'Microsoft.Teams.Api.Activities.Invokes.TaskActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToTask());
+        Assert.Equal(expectedSubmitException, ex.Message);
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonDeserialize_Derived()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigSubmitActivity.json");
+        var activity = JsonSerializer.Deserialize<ConfigActivity>(json);
+        var expected = setupConfigSubmitActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+        Assert.NotNull(activity.ToSubmit());
+        var expectedSubmitException = "Unable to cast object of type 'SubmitActivity' to type 'FetchActivity'.";
+        var ex = Assert.Throws<System.InvalidCastException>(() => activity.ToFetch());
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonDeserialize_Derived_Interface()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigSubmitActivity.json");
+        var activity = JsonSerializer.Deserialize<InvokeActivity>(json);
+        var expected = setupConfigSubmitActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+    }
+
+    [Fact]
+    public void ConfigSubmitActivity_JsonDeserialize_Derived_Activity_Interface()
+    {
+        var json = File.ReadAllText(@"../../../Json/Activity/Invokes/ConfigSubmitActivity.json");
+        var activity = JsonSerializer.Deserialize<Activity>(json);
+        var expected = setupConfigSubmitActivity();
+
+        Assert.NotNull(activity);
+        Assert.Equal(expected.ToString(), activity.ToString());
+    }
+}
