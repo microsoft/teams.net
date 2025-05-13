5cc342462567642e39bd957c1fc4140bd11dbd21
Alex Acebo <aacebowork@gmail.com>
2025-05-13 10:35:32 -0700

    add adaptive card action invoke

---
+++ Libraries/Microsoft.Teams.Apps/Activities/Invokes/AdaptiveCards/ActionActivity.cs
@@ -0,0 +1,36 @@
+using Microsoft.Teams.Api.Activities;
+using Microsoft.Teams.Api.Activities.Invokes;
+using Microsoft.Teams.Apps.Routing;
+
+namespace Microsoft.Teams.Apps.Activities.Invokes;
+
+public static partial class AdaptiveCard
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class ActionAttribute() : InvokeAttribute(Api.Activities.Invokes.Name.AdaptiveCards.Action, typeof(AdaptiveCards.ActionActivity))
+    {
+        public override object Coerce(IContext<IActivity> context) => context.ToActivityType<AdaptiveCards.ActionActivity>();
+    }
+}
+
+public static partial class AppInvokeActivityExtensions
+{
+    public static App OnAdaptiveCardAction(this App app, Func<IContext<AdaptiveCards.ActionActivity>, Task<object?>> handler)
+    {
+        app.Router.Register(new Route()
+        {
+            Handler = context => handler(context.ToActivityType<AdaptiveCards.ActionActivity>()),
+            Selector = activity =>
+            {
+                if (activity is AdaptiveCards.ActionActivity)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        return app;
+    }
+}
