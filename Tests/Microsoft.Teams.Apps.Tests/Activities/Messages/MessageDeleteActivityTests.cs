ee18dd5cc49334cc80666088719d51e55e94db6f
Alex Acebo <aacebowork@gmail.com>
2025-05-13 09:09:20 -0700

    add some tests

--- Tests/Microsoft.Teams.Apps.Tests/Activities/Messages/MessageDeleteActivityTests.cs
+++ Tests/Microsoft.Teams.Apps.Tests/Activities/Messages/MessageDeleteActivityTests.cs
@@ -1,20 +1,21 @@
 using Microsoft.Teams.Api.Activities;
 using Microsoft.Teams.Api.Auth;
 using Microsoft.Teams.Apps.Activities;
+using Microsoft.Teams.Apps.Annotations;
 using Microsoft.Teams.Apps.Testing.Plugins;
 
 namespace Microsoft.Teams.Apps.Tests.Activities;
 
 public class MessageDeleteActivityTests
 {
-    private readonly App _app;
-    private readonly IToken _token;
+    private readonly App _app = new();
+    private readonly IToken _token = Globals.Token;
+    private readonly Controller _controller = new();
 
     public MessageDeleteActivityTests()
     {
-        _app = new App();
         _app.AddPlugin(new TestPlugin());
-        _token = Globals.Token;
+        _app.AddController(_controller);
     }
 
     [Fact]
@@ -40,6 +41,7 @@ public class MessageDeleteActivityTests
 
         Assert.Equal(System.Net.HttpStatusCode.OK, res.Status);
         Assert.Equal(2, calls);
+        Assert.Equal(1, _controller.Calls);
     }
 
     [Fact]
@@ -58,5 +60,19 @@ public class MessageDeleteActivityTests
 
         Assert.Equal(System.Net.HttpStatusCode.OK, res.Status);
         Assert.Equal(0, calls);
+        Assert.Equal(0, _controller.Calls);
+    }
+
+    [TeamsController]
+    public class Controller
+    {
+        public int Calls { get; private set; } = 0;
+
+        [Message.Delete]
+        public void OnMessageDelete([Context] IContext.Next next)
+        {
+            Calls++;
+            next();
+        }
     }
 }
