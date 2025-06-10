27ed7032b4d63db0afb222923f797a97bb4576d7
Falguni Thakor <fthakor@microsoft.com>
2025-06-10 15:34:41 -0700

    sample app code for ME

---
+++ Samples/Samples.MessageExtensions/HomeController.cs
@@ -0,0 +1,62 @@
+
+
+using Microsoft.Teams.Api.MessageExtensions;
+using Microsoft.Teams.Apps;
+using Microsoft.Teams.Apps.Activities;
+using Microsoft.Teams.Apps.Activities.Invokes;
+using Microsoft.Teams.Apps.Annotations;
+
+
+namespace Samples.MessageExtensions;
+public class HomeController 
+{
+    [Message]
+    public async Task OnMessage([Context] Microsoft.Teams.Api.Activities.MessageActivity activity, [Context] IContext.Client client)
+    {
+        await client.Send($"you said \"{activity.Text}\"");
+    }
+
+    [MessageExtension.SubmitAction]
+    public Task<Microsoft.Teams.Api.MessageExtensions.Response> OnSubmitAction([Context] Microsoft.Teams.Api.Activities.Invokes.MessageExtensions.SubmitActionActivity action, [Context] IContext.Client client)
+    {
+        var commanndId = action.Value.CommandId;
+
+        if (string.IsNullOrEmpty(commanndId))
+        {
+            // context.Log.Error("No command ID provided for submit action.");
+            return Task.FromResult<Microsoft.Teams.Api.MessageExtensions.Response>(null);
+        }
+
+        if (commanndId == "createCard")
+        {
+            // context.Log.Info("createCard response to submit action.");
+            // todo: create card
+        }
+        else if (commanndId == "getMessageDetails" && action.Value.MessagePayload is not null)
+        {
+            // context.Log.Info("getMessageDetails response to submit action.");
+            // TODO: create message details card
+        }
+        else
+        {
+            // context.Log.Error($"Unknown command ID: {commanndId}");
+            return Task.FromResult<Microsoft.Teams.Api.MessageExtensions.Response>(null);
+        }
+
+        return Task.FromResult(new Microsoft.Teams.Api.MessageExtensions.Response()
+        {
+            ComposeExtension = new Result()
+            {
+                Attachments = new List<Microsoft.Teams.Api.MessageExtensions.Attachment>()
+                {
+                    new Microsoft.Teams.Api.MessageExtensions.Attachment()
+                    {
+                        Content = "Your updated content here",
+                        Name = "Your updated attachment name",
+                    }
+                },
+                AttachmentLayout = Microsoft.Teams.Api.Attachment.Layout.List,
+            }
+        });
+    }
+}
---
+++ Samples/Samples.MessageExtensions/Program.cs
@@ -0,0 +1,152 @@
+
+using Microsoft.Teams.Api.MessageExtensions;
+using Microsoft.Teams.Apps.Activities.Invokes;
+using Microsoft.Teams.Apps.Extensions;
+using Microsoft.Teams.Cards;
+using Microsoft.Teams.Plugins.AspNetCore.Extensions;
+
+using Samples.MessageExtensions;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<HomeController>();
+builder.AddTeams(Microsoft.Teams.Apps.App.Builder().AddLogger(level: Microsoft.Teams.Common.Logging.LogLevel.Debug));
+
+var app = builder.Build();
+var teams = app.UseTeams();
+
+
+teams.OnQueryLink(async context => {
+    context.Log.Info("OnQueryLink triggered:");
+    var url = context.Activity.Value.Url;
+
+    if (string.IsNullOrEmpty(url))
+    {
+        context.Log.Error("No URL provided for link unfurling.");
+        return null;
+    }
+
+    var card = new Microsoft.Teams.Cards.AdaptiveCard
+    {
+        Body = new List<CardElement>
+            {
+                new TextBlock("Hello from Samples.Agent!")
+                {
+                    Size = TextSize.Large,
+                    Weight = TextWeight.Bolder
+                },
+                new TextBlock("This is a sample card created with the CreateCard method.") {
+                    Wrap = true
+                }
+            },
+    };
+
+    var response = new Microsoft.Teams.Api.MessageExtensions.Response
+    {
+        ComposeExtension = new Result
+        {
+            Attachments = new List<Microsoft.Teams.Api.MessageExtensions.Attachment>
+               {
+                    new Microsoft.Teams.Api.MessageExtensions.Attachment()
+                    {
+                        Content = card,
+                        Name = "Sample Card",
+                    }
+               },
+            AttachmentLayout = Microsoft.Teams.Api.Attachment.Layout.List,
+        }
+    };
+
+    return response;
+});
+
+teams.OnMessageExtensionSubmitAction(async context =>
+{
+    context.Log.Info("OnSubmitAction triggered:");
+
+    var commanndId = context.Activity.Value.CommandId;
+
+    if (string.IsNullOrEmpty(commanndId))
+    {
+        context.Log.Error("No command ID provided for submit action.");
+        return null;
+    }
+
+    if (commanndId == "createCard")
+    {
+        context.Log.Info("createCard response to submit action.");
+        // todo: create card
+
+    }
+    else if (commanndId == "getMessageDetails" && context.Activity.Value.MessagePayload is not null)
+    {
+        context.Log.Info("getMessageDetails response to submit action.");
+        // TODO: create message details card
+    }
+    else
+    {
+        context.Log.Error($"Unknown command ID: {commanndId}");
+        return null;
+    }
+    return new Response()
+    {
+        ComposeExtension = new Result()
+        {
+            Attachments = new List<Microsoft.Teams.Api.MessageExtensions.Attachment>()
+            {
+                new Microsoft.Teams.Api.MessageExtensions.Attachment()
+                {
+                    Content = "Your updated content here",
+                    Name = "Your updated attachment name",
+                }
+            },
+            AttachmentLayout = Microsoft.Teams.Api.Attachment.Layout.List,
+        }
+    };
+
+});
+
+
+
+teams.OnQuery(async context => {
+    context.Log.Info("OnQuery triggered:");
+
+    var query = context.Activity.Value;
+    if (query.CommandId == "searchQuery")
+    {
+        var card = new Microsoft.Teams.Cards.AdaptiveCard
+        {
+            Body = new List<CardElement>
+            {
+                new TextBlock("Samples.Agent search query!")
+                {
+                    Size = TextSize.Large,
+                    Weight = TextWeight.Bolder
+                },
+                new TextBlock("This is a sample card created with the CreateCard method.") {
+                    Wrap = true
+                }
+            },
+        };
+        return new Response()
+        {
+            ComposeExtension = new Result()
+            {
+                Attachments = new List<Microsoft.Teams.Api.MessageExtensions.Attachment>()
+            {
+                new Microsoft.Teams.Api.MessageExtensions.Attachment()
+                {
+                    Content = "Your updated content here",
+                    Name = "Your updated attachment name",
+                }
+            },
+                AttachmentLayout = Microsoft.Teams.Api.Attachment.Layout.List,
+            }
+        };
+    }
+    return null;
+
+});
+
+app.Run();
