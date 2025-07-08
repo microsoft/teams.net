669e6bf0cc7a305df750d58e3517d59222aca9dc
Alex Acebo <aacebowork@gmail.com>
2025-07-08 13:25:31 -0700

    add token verify extension

--- Libraries/Microsoft.Teams.Extensions/Microsoft.Teams.Extensions.Configuration/Microsoft.Teams.Apps.Extensions/TeamsSettings.cs
+++ Libraries/Microsoft.Teams.Extensions/Microsoft.Teams.Extensions.Configuration/Microsoft.Teams.Apps.Extensions/TeamsSettings.cs
@@ -10,6 +10,7 @@ public class TeamsSettings
     public string? ClientId { get; set; }
     public string? ClientSecret { get; set; }
     public string? TenantId { get; set; }
+    public ActivitySettings Activity { get; set; } = new();
 
     public bool Empty
     {
@@ -26,5 +27,20 @@ public class TeamsSettings
         }
 
         return options;
+    }
+
+    public class ActivitySettings
+    {
+        public string OpenIdMetadataUrl = "https://login.botframework.com/v1/.well-known/openidconfiguration";
+        public List<string> Audiences = [];
+        public List<string> Issuers = [
+            "https://api.botframework.com",
+            "https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/",
+            "https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0",
+            "https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/",
+            "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0",
+            "https://sts.windows.net/69e9b82d-4842-4902-8d1e-abc5b98a55e8/",
+            "https://login.microsoftonline.com/69e9b82d-4842-4902-8d1e-abc5b98a55e8/v2.0",
+        ];
     }
 }
--- Libraries/Microsoft.Teams.Plugins/Microsoft.Teams.Plugins.AspNetCore/Extensions/HostApplicationBuilder.cs
+++ Libraries/Microsoft.Teams.Plugins/Microsoft.Teams.Plugins.AspNetCore/Extensions/HostApplicationBuilder.cs
@@ -1,10 +1,17 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Concurrent;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Validators;
 using Microsoft.Teams.Apps;
 using Microsoft.Teams.Apps.Extensions;
 
@@ -12,6 +19,8 @@ namespace Microsoft.Teams.Plugins.AspNetCore.Extensions;
 
 public static class HostApplicationBuilderExtensions
 {
+    private static readonly ConcurrentDictionary<string, ConfigurationManager<OpenIdConnectConfiguration>> _openIdMetadataCache = new();
+
     public static IHostApplicationBuilder AddTeams(this IHostApplicationBuilder builder)
     {
         builder.AddTeamsCore();
@@ -41,6 +50,75 @@ public static class HostApplicationBuilderExtensions
         builder.AddTeamsCore(appBuilder);
         builder.AddTeamsPlugin<AspNetCorePlugin>();
         builder.Services.AddControllers().AddApplicationPart(Assembly.GetExecutingAssembly());
+        return builder;
+    }
+
+    public static IHostApplicationBuilder AddTeamsTokenAuthentication(this IHostApplicationBuilder builder)
+    {
+        var settings = builder.Configuration.GetTeams();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.TokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                RequireSignedTokens = true,
+                ClockSkew = TimeSpan.FromMinutes(5),
+                ValidIssuers = settings.Activity.Issuers,
+                ValidAudiences = settings.Activity.Audiences,
+            };
+
+            options.TokenValidationParameters.EnableAadSigningKeyIssuerValidation();
+            options.Events = new()
+            {
+                OnMessageReceived = async context =>
+                {
+                    var header = context.Request.Headers.Authorization.ToString();
+
+                    if (string.IsNullOrEmpty(header))
+                    {
+                        context.Options.TokenValidationParameters.ConfigurationManager ??= options.ConfigurationManager as BaseConfigurationManager;
+                        await Task.CompletedTask.ConfigureAwait(false);
+                        return;
+                    }
+
+                    var parts = header.Split(' ');
+
+                    if (parts.Length != 2 || parts.First() != "Bearer")
+                    {
+                        context.Options.TokenValidationParameters.ConfigurationManager ??= options.ConfigurationManager as BaseConfigurationManager;
+                        await Task.CompletedTask.ConfigureAwait(false);
+                        return;
+                    }
+
+                    var token = new JwtSecurityToken(parts[1]);
+                    var issuer = token.Claims.FirstOrDefault(claim => claim.Type == "iss")?.Value;
+
+                    if (issuer == "https://api.botframework.com")
+                    {
+                        context.Options.TokenValidationParameters.ConfigurationManager = _openIdMetadataCache.GetOrAdd(settings.Activity.OpenIdMetadataUrl, key =>
+                        {
+                            return new ConfigurationManager<OpenIdConnectConfiguration>(settings.Activity.OpenIdMetadataUrl, new OpenIdConnectConfigurationRetriever(), new HttpClient())
+                            {
+                                AutomaticRefreshInterval = BaseConfigurationManager.DefaultAutomaticRefreshInterval
+                            };
+                        });
+                    }
+
+                    await Task.CompletedTask.ConfigureAwait(false);
+                },
+            };
+        });
+
         return builder;
     }
 }
