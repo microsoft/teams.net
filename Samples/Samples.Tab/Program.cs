c276f4633f2accd41266c10fbc2ecf0cdb7e87f2
Alex Acebo <aacebowork@gmail.com>
2025-09-04 14:52:30 -0700

    add web folder to tab sample

--- Samples/Samples.Tab/Program.cs
+++ Samples/Samples.Tab/Program.cs
@@ -7,5 +7,5 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddTeams().AddTeamsDevTools();
 var app = builder.Build();
 
-app.UseTeams();
+app.AddTeamsTab("test", "Web/dist").UseTeams();
 app.Run();
