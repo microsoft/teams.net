436972edd2a63f773497d45cdc1de6342c7a1c20
Jesperholmbergmsft <202218569+Jesperholmbergmsft@users.noreply.github.com>
2025-09-16 14:23:10 -0700

    Entra token validation for remote function calling (#124)

--- Libraries/Microsoft.Teams.Plugins/Microsoft.Teams.Plugins.AspNetCore/Extensions/HostApplicationBuilder.cs
+++ Libraries/Microsoft.Teams.Plugins/Microsoft.Teams.Plugins.AspNetCore/Extensions/HostApplicationBuilder.cs
@@ -104,6 +104,12 @@ public static class HostApplicationBuilderExtensions
         public const string AuthorizationPolicy = "TeamsJWTPolicy";
     }
 
+    public static class EntraTokenAuthConstants
+    {
+        public const string AuthenticationScheme = "EntraTokenJWTScheme";
+        public const string AuthorizationPolicy = "EntraTokenJWTPolicy"; 
+    }
+
     /// <summary>
     /// adds authentication and authorization to validate incoming Teams tokens
     /// </summary>
@@ -118,15 +124,22 @@ public static class HostApplicationBuilderExtensions
             teamsValidationSettings.AddDefaultAudiences(settings.ClientId);
         }
 
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(TeamsTokenAuthConstants.AuthenticationScheme, options =>
-        {
-            TokenValidator.ConfigureValidation(options, teamsValidationSettings.Issuers, teamsValidationSettings.Audiences, teamsValidationSettings.OpenIdMetadataUrl);
-        });
+        builder.Services.
+            AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(TeamsTokenAuthConstants.AuthenticationScheme, options =>
+            {
+                TokenValidator.ConfigureValidation(options, teamsValidationSettings.Issuers, teamsValidationSettings.Audiences, teamsValidationSettings.OpenIdMetadataUrl);
+            })
+            .AddJwtBearer(EntraTokenAuthConstants.AuthenticationScheme, options =>
+            {
+                TokenValidator.ConfigureValidation(options, teamsValidationSettings.GetValidIssuersForTenant(settings.TenantId), teamsValidationSettings.Audiences, teamsValidationSettings.GetTenantSpecificOpenIdMetadataUrl(settings.TenantId));
+            });
+
 
         // add [Authorize(Policy="..")] support for endpoints
         builder.Services.AddAuthorization(options =>
         {
+            // token validation policy for SMBA tokens
             options.AddPolicy(TeamsTokenAuthConstants.AuthorizationPolicy, policy =>
             {
                 if (skipAuth || string.IsNullOrEmpty(settings.ClientId))
@@ -139,6 +152,13 @@ public static class HostApplicationBuilderExtensions
                     policy.AddAuthenticationSchemes(TeamsTokenAuthConstants.AuthenticationScheme);
                     policy.RequireAuthenticatedUser();
                 }
+            });
+
+            // token validation policy for Entra tokens, used when tab apps invoke remote functions
+            options.AddPolicy(EntraTokenAuthConstants.AuthorizationPolicy, policy =>
+            {
+                policy.AddAuthenticationSchemes(EntraTokenAuthConstants.AuthenticationScheme);
+                policy.RequireAuthenticatedUser();
             });
         });
 
