de5dffc20a7247b448d3d20ed54fa8a20a8c0e7e
Alex Acebo <aacebowork@gmail.com>
2025-10-31 09:22:39 -0700

    Update Controller.cs

--- Samples/Samples.Agents/Controller.cs
+++ Samples/Samples.Agents/Controller.cs
@@ -2,6 +2,7 @@ using Microsoft.Teams.Api.Activities;
 using Microsoft.Teams.Apps;
 using Microsoft.Teams.Apps.Activities;
 using Microsoft.Teams.Apps.Annotations;
+using Microsoft.Teams.Plugins.Agents.Extensions;
 
 namespace Samples.Agents;
 
@@ -9,8 +10,9 @@ namespace Samples.Agents;
 public class Controller
 {
     [Message]
-    public async Task OnMessage([Context] MessageActivity activity, [Context] IContext.Client client)
+    public async Task OnMessage(IContext<MessageActivity> context, [Context] MessageActivity activity, [Context] IContext.Client client)
     {
+        await context.GetTurnContext().SendActivityAsync("Teams Application => hi from the turn context!");
         await client.Send($"Teams Application => you said '{activity.Text}'");
     }
 }
