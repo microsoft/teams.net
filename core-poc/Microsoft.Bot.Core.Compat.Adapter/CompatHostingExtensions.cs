e0362e4826714509397911f258d99749b8ffe12b
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-12-05 08:53:10 -0800

    addcompatadapter

---
+++ core-poc/Microsoft.Bot.Core.Compat.Adapter/CompatHostingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Bot.Core.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace Microsoft.Bot.Core.Compat.Adapter;
+
+public static class CompatHostingExtensions
+{
+    public static IHostApplicationBuilder AddCompatAdapter(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddBotApplication<BotApplication>();
+        builder.Services.AddSingleton<CompatBotAdapter>();
+        builder.Services.AddSingleton<IBotFrameworkHttpAdapter, CompatAdapter>();
+        return builder;
+    }
+}
--- core-poc/compat/Program.cs
+++ core-poc/compat/Program.cs
@@ -2,20 +2,12 @@ using System.Collections.Concurrent;
 
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Core;
 using Microsoft.Bot.Core.Compat.Adapter;
 using Microsoft.Bot.Schema;
 
-using Microsoft.Bot.Core.Hosting;
-using compat;
-
 WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(args);
-webAppBuilder.Services.AddBotApplication<BotApplication>();
-
-webAppBuilder.Services.AddSingleton<CompatBotAdapter>();
-webAppBuilder.Services.AddSingleton<IBotFrameworkHttpAdapter, CustomAdapter>();
+webAppBuilder.AddCompatAdapter();
 webAppBuilder.Services.AddSingleton<IBot, EchoBot>();
-
 webAppBuilder.Services.AddSingleton<ConcurrentDictionary<string, ConversationReference>>();
 
 WebApplication webApp = webAppBuilder.Build();
