a6b2cb51f2e44c1bef5a29419cad4d5f7d3aa0a6
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-12-19 21:32:31 -0800

    Add hello-assistant scenario with basic message handling

---
+++ core/samples/scenarios/hello-assistant.cs
@@ -0,0 +1,34 @@
+#!/usr/bin/dotnet run
+
+#:sdk Microsoft.NET.Sdk.Web
+
+#:project ../../src/Microsoft.Bot.Core/Microsoft.Bot.Core.csproj
+#:project ../../src/Microsoft.Teams.BotApps/Microsoft.Teams.BotApps.csproj
+
+
+using Microsoft.Teams.BotApps;
+using Microsoft.Teams.BotApps.Schema;
+using Microsoft.Teams.BotApps.Schema.Entities;
+
+var builder = TeamsBotApplication.CreateBuilder();
+var teamsApp = builder.Build();
+
+teamsApp.OnMessage = async (context, cancellationToken) =>
+{
+    string replyText = $"You sent: `{context.Activity.Text}` in activity of type `{context.Activity.Type}`.";
+
+    // await context.SendTypingActivityAsync(cancellationToken);
+
+    // TeamsActivity reply = TeamsActivity.CreateBuilder()
+    //     .WithType(TeamsActivityTypes.Message)
+    //     .WithConversationReference(context.Activity)
+    //     .WithText(replyText)
+    //     .Build();
+
+
+    // reply.AddMention(context.Activity.From!, "ridobotlocal", true);
+
+    await context.SendActivityAsync(replyText, cancellationToken);
+};
+
+teamsApp.Run();
