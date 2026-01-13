cc976be72115ee6345842f897328f5c7bc72b033
Mehak Bindra <mehakbindra@microsoft.com>
2026-01-12 20:49:30 -0800

    fix teamsbotapp

--- core/src/Microsoft.Teams.BotApps/TeamsBotApplication.cs
+++ core/src/Microsoft.Teams.BotApps/TeamsBotApplication.cs
@@ -49,7 +49,7 @@ public class TeamsBotApplication : BotApplication
     /// <param name="httpContextAccessor"></param>
     /// <param name="logger"></param>
     /// <param name="sectionName"></param>
-    public TeamsBotApplication(ConversationClient conversationClient, UserTokenClient userTokenClient, IConfiguration config, ILogger<BotApplication> logger, string sectionName = "AzureAd") : base(conversationClient, userTokenClient, config, logger, sectionName)
+    public TeamsBotApplication(ConversationClient conversationClient, UserTokenClient userTokenClient, IConfiguration config, IHttpContextAccessor httpContextAccessor, ILogger<BotApplication> logger, string sectionName = "AzureAd") : base(conversationClient, userTokenClient, config, logger, sectionName)
     {
         OnActivity = async (activity, cancellationToken) =>
         {
