e0f48ad6e0e0268297048c3387f19e3eb49bd632
Kavin <115390646+singhk97@users.noreply.github.com>
2026-01-28 12:36:38 -0800

    Update Program.cs

--- Samples/Samples.Echo/Program.cs
+++ Samples/Samples.Echo/Program.cs
@@ -18,7 +18,7 @@ teams.OnMessage(async context =>
 {
     context.Log.Info("hit!");
     await context.Typing();
-    await context.Send($"you said '{context.Activity.Text}'4");
+    await context.Send($"you said '{context.Activity.Text}'5");
 });
 
 app.Run();
