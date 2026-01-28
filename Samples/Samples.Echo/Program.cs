3eff048cc9583aaf7016dc86b25bd9afc1206b54
Kavin <115390646+singhk97@users.noreply.github.com>
2026-01-28 12:05:38 -0800

    Update message response text in Program.cs

--- Samples/Samples.Echo/Program.cs
+++ Samples/Samples.Echo/Program.cs
@@ -18,7 +18,7 @@ teams.OnMessage(async context =>
 {
     context.Log.Info("hit!");
     await context.Typing();
-    await context.Send($"you said '{context.Activity.Text}'2");
+    await context.Send($"you said '{context.Activity.Text}'3");
 });
 
 app.Run();
