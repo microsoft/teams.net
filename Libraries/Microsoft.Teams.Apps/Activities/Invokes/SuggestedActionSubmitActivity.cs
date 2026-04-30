e1269b9ff7c2ceb38f37178bd20774572af67411
Kavin Singh <kavinsingh@microsoft.com>
2026-04-30 15:52:03 -0400

    refactor: drop SuggestedActionSubmitAttribute and obsolete OnSuggestedActionSubmit overloads

--- Libraries/Microsoft.Teams.Apps/Activities/Invokes/SuggestedActionSubmitActivity.cs
+++ Libraries/Microsoft.Teams.Apps/Activities/Invokes/SuggestedActionSubmitActivity.cs
@@ -7,46 +7,8 @@ using Microsoft.Teams.Apps.Routing;
 
 namespace Microsoft.Teams.Apps.Activities.Invokes;
 
-[AttributeUsage(AttributeTargets.Method, Inherited = true)]
-public class SuggestedActionSubmitAttribute() : InvokeAttribute(Api.Activities.Invokes.Name.SuggestedActionSubmit, typeof(SuggestedActionSubmitActivity))
-{
-    public override object Coerce(IContext<IActivity> context) => context.ToActivityType<SuggestedActionSubmitActivity>();
-}
-
 public static partial class AppInvokeActivityExtensions
 {
-    [Obsolete("Use the handler with the cancellation token")]
-    public static App OnSuggestedActionSubmit(this App app, Func<IContext<SuggestedActionSubmitActivity>, Task> handler)
-    {
-        app.Router.Register(new Route()
-        {
-            Name = string.Join("/", [ActivityType.Invoke, Name.SuggestedActionSubmit]),
-            Type = app.Status is null ? RouteType.System : RouteType.User,
-            Handler = async context =>
-            {
-                await handler(context.ToActivityType<SuggestedActionSubmitActivity>());
-                return null;
-            },
-            Selector = activity => activity is SuggestedActionSubmitActivity
-        });
-
-        return app;
-    }
-
-    [Obsolete("Use the handler with the cancellation token")]
-    public static App OnSuggestedActionSubmit(this App app, Func<IContext<SuggestedActionSubmitActivity>, Task<object?>> handler)
-    {
-        app.Router.Register(new Route()
-        {
-            Name = string.Join("/", [ActivityType.Invoke, Name.SuggestedActionSubmit]),
-            Type = app.Status is null ? RouteType.System : RouteType.User,
-            Handler = context => handler(context.ToActivityType<SuggestedActionSubmitActivity>()),
-            Selector = activity => activity is SuggestedActionSubmitActivity
-        });
-
-        return app;
-    }
-
     public static App OnSuggestedActionSubmit(this App app, Func<IContext<SuggestedActionSubmitActivity>, CancellationToken, Task> handler)
     {
         app.Router.Register(new Route()
