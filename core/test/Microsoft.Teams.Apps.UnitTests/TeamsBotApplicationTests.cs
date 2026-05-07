c6ea5a46ff343fef925a736a00d9f7fbaf9854f1
Corina Gum <14900841+corinagum@users.noreply.github.com>
2026-05-07 12:10:38 -0700

    Unit tests

---
+++ core/test/Microsoft.Teams.Apps.UnitTests/TeamsBotApplicationTests.cs
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Teams.Apps.Api.Clients;
+using Microsoft.Teams.Core;
+using Microsoft.Teams.Core.Hosting;
+using Moq;
+
+namespace Microsoft.Teams.Apps.UnitTests;
+
+public class TeamsBotApplicationTests
+{
+    [Fact]
+    public async Task Reply_Proactive_ThrowsOnInvalidMessageId()
+    {
+        TeamsBotApplication app = CreateApp();
+
+        await Assert.ThrowsAsync<ArgumentException>(() =>
+            app.Reply("19:abc@thread.skype", "not-a-number", "hello"));
+    }
+
+    [Fact]
+    public async Task Reply_Proactive_ThrowsOnZeroMessageId()
+    {
+        TeamsBotApplication app = CreateApp();
+
+        await Assert.ThrowsAsync<ArgumentException>(() =>
+            app.Reply("19:abc@thread.skype", "0", "hello"));
+    }
+
+    [Fact]
+    public async Task Reply_Proactive_ThrowsOnEmptyConversationId()
+    {
+        TeamsBotApplication app = CreateApp();
+
+        await Assert.ThrowsAsync<ArgumentException>(() =>
+            app.Reply("", "1680000000000", "hello"));
+    }
+
+    private static TeamsBotApplication CreateApp()
+    {
+        Mock<UserTokenClient> mockUserTokenClient = new(
+            new HttpClient(),
+            new Mock<IConfiguration>().Object,
+            NullLogger<UserTokenClient>.Instance);
+
+        Mock<ConversationClient> mockConversationClient = new(
+            new HttpClient(),
+            NullLogger<ConversationClient>.Instance);
+
+        ApiClient apiClient = new(
+            new HttpClient(),
+            mockConversationClient.Object,
+            mockUserTokenClient.Object);
+
+        return new TeamsBotApplication(
+            mockConversationClient.Object,
+            mockUserTokenClient.Object,
+            apiClient,
+            new HttpContextAccessor(),
+            NullLogger<TeamsBotApplication>.Instance,
+            new BotApplicationOptions { AppId = "test-app-id" });
+    }
+}
---
+++ core/test/Microsoft.Teams.Core.UnitTests/Schema/ConversationTests.cs
@@ -0,0 +1,86 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Teams.Core.Schema;
+
+namespace Microsoft.Teams.Core.UnitTests.Schema;
+
+public class ConversationTests
+{
+    [Fact]
+    public void ToThreadedConversationId_ConstructsThreadedConversationId()
+    {
+        var result = Conversation.ToThreadedConversationId("19:abc@thread.skype", "1680000000000");
+        Assert.Equal("19:abc@thread.skype;messageid=1680000000000", result);
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_WorksWithDifferentConversationIdFormats()
+    {
+        var result = Conversation.ToThreadedConversationId("19:meeting_abc@thread.v2", "999");
+        Assert.Equal("19:meeting_abc@thread.v2;messageid=999", result);
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnEmptyConversationId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("", "123"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnNullConversationId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId(null!, "123"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnEmptyMessageId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("19:abc@thread.skype", ""));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnZeroMessageId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("19:abc@thread.skype", "0"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnNonNumericMessageId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("19:abc@thread.skype", "abc"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnNegativeMessageId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("19:abc@thread.skype", "-1"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_ThrowsOnDecimalMessageId()
+    {
+        Assert.Throws<ArgumentException>(() => Conversation.ToThreadedConversationId("19:abc@thread.skype", "1.5"));
+    }
+
+    [Fact]
+    public void ToThreadedConversationId_StripsExistingMessageIdAndReplacesWithThreadRoot()
+    {
+        var result = Conversation.ToThreadedConversationId("19:abc@thread.skype;messageid=111", "222");
+        Assert.Equal("19:abc@thread.skype;messageid=222", result);
+    }
+
+    [Fact]
+    public void ThreadId_StripsMessageIdSuffix()
+    {
+        var conv = new Conversation("19:abc@thread.skype;messageid=1680000000000");
+        Assert.Equal("19:abc@thread.skype", conv.ThreadId);
+    }
+
+    [Fact]
+    public void ThreadId_ReturnsIdWhenNoSuffix()
+    {
+        var conv = new Conversation("19:abc@thread.skype");
+        Assert.Equal("19:abc@thread.skype", conv.ThreadId);
+    }
+}
