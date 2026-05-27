dfd10284595279e6c1457e82dca8b003c3d32c49
MehakBindra <mhk0397@gmail.com>
2026-05-27 16:37:41 -0700

    fix

--- core/test/Microsoft.Teams.Apps.UnitTests/TeamsBotApplicationHostingExtensionsTests.cs
+++ core/test/Microsoft.Teams.Apps.UnitTests/TeamsBotApplicationHostingExtensionsTests.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Teams.Apps.Api.Clients;
+using Microsoft.Teams.Core.Hosting;
 
 namespace Microsoft.Teams.Apps.UnitTests;
 
